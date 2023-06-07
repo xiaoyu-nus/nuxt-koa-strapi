47c83b07f9768122a57b2f073977f12c429ab9e8
xiaoyu <tkgs.liu.xiaoyu@gmail.com>
2023-06-07 17:56:47 +0800

    enlist wallets

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
   <h1 v-if="wallets && !wallets.length"> Hello World! </h1>
-    <div v-else v-for="wallet in wallets" v-bind:key="wallet.slug">
-      <h2> {{ wallet.wallet_name }} </h2>
+  <div v-else>
+    <h1>Wallets</h1>
+    <div v-for="wallet in wallets" v-bind:key="wallet.slug">
+      <h2> {{ wallet.attributes.wallet_name }} </h2>
     </div>
+  </div>
   </div>
 </template>
 
@@ -25,11 +28,14 @@ export default {
   },
   created: async function () {
     console.log("created!!")
-    const variables = { limit: 5 };
-    const response = await useAsyncQuery(allWalletsQuery, variables);
-    this.wallets = response.data._rawValue.wallets.data;
-    console.log(response.data._rawValue.wallets.data);
+    const { data } = await useAsyncQuery(allWalletsQuery);
+    if (data.value?.wallets) {
+      // log response
+      console.log("received wallets", data.value.wallets.data)
+      this.wallets = data.value.wallets.data;
+    }
     
+
   }
 }
 </script> 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
 	modules: [
 		`@nuxtjs/apollo`,
 	],	
+	
 	apollo: {
 		clients: {
       default: {
